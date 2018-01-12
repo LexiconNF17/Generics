0d95e3d8dda2ea66cd725abfedca6231dfce5093
Adrian Lozano <adrian@xenotype.com>
2018-01-12 12:04:02 +0100

    Add project files.

---
+++ Generics/Pair.cs
@@ -0,0 +1,30 @@
+﻿namespace Generics
+{
+    class Pair : Pair<int>
+    {
+        public Pair(int first, int second) : base(first, second) { }
+    }
+
+    class Pair<T> : Pair<T, T>
+    {
+        public Pair(T first, T second) : base(first, second) { }
+    }
+
+    class Pair<TFirst, TSecond>
+    {
+        public TFirst First { get; set; }
+        public TSecond Second { get; set; }
+
+        public Pair(TFirst first, TSecond second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public override string ToString()
+        {
+            return "[" + First.ToString()
+                + ", " + Second.ToString() + "]";
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Pair<int>(4, 6);
+            Console.WriteLine(p.First);
+            Console.WriteLine(p.Second);
+            Console.WriteLine(p);
+            Console.WriteLine();
+
+            var p2 = new Pair<string>("four", "six");
+            Console.WriteLine(p2.First);
+            Console.WriteLine(p2.Second);
+            Console.WriteLine(p2);
+            Console.WriteLine();
+
+            var p3 = new Pair<int, string>(3, "three");
+            Console.WriteLine(p3.First);
+            Console.WriteLine(p3.Second);
+            Console.WriteLine(p3);
+            Console.WriteLine();
+
+            var p4 = new Pair(4, 6);
+            Console.WriteLine(p4.First);
+            Console.WriteLine(p4.Second);
+            Console.WriteLine(p4);
+            Console.WriteLine();
+
+        }
+    }
+}
